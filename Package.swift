5e14ced6e3e3aed1e965008bee7cac1ea3cce22b
Ariesta <ariesta.work@gmail.com>
2024-03-13 09:19:10 +0700

    Update dependency

--- Package.swift
+++ Package.swift
@@ -11,11 +11,22 @@ let package = Package(
             name: "HomeModule",
             targets: ["HomeModule"]),
     ],
+    dependencies: [
+        .package(path: "/BalanceCore"),
+        .package(path: "/ScanQRModule"),
+        .package(path: "/PaymentHistoryModule"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "HomeModule"),
+            name: "HomeModule",
+            dependencies: [
+                "BalanceCore",
+                "ScanQRModule",
+                "PaymentHistoryModule"
+            ]
+        ),
         .testTarget(
             name: "HomeModuleTests",
             dependencies: ["HomeModule"]),
